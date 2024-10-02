c71ab67f8a54e18dcc50b3d419a08fbdf5ea3577
raygunna <gunnarsray@gmail.com>
2024-10-02 15:26:30 -0500

    Updated populateCells() to iterate over a list

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -5,6 +5,8 @@ import edu.macalester.graphics.Ellipse;
 import edu.macalester.graphics.Point;
 
 import java.awt.Color;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
 
 @SuppressWarnings("SameParameterValue")
@@ -12,7 +14,7 @@ public class CellSimulation {
 
     private CanvasWindow canvas;
     private Random rand = new Random();
-    private Cell cell;
+    private List<Cell> cells;
 
     public static void main(String[] args) {
         new CellSimulation();
@@ -25,8 +27,11 @@ public class CellSimulation {
         //noinspection InfiniteLoopStatement
         while (true) {
             Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            cell.moveAround(canvasCenter);
-            cell.grow(0.02);
+            
+            for (Cell cell : cells) {
+                cell.moveAround(canvasCenter);
+                cell.grow(0.02);
+            }
 
             canvas.draw();
             canvas.pause(10);
@@ -35,12 +40,16 @@ public class CellSimulation {
 
     private void populateCells() {
         double size = rand.nextInt(5) + 2;
-        cell = new Cell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(cell.getShape());
+        cells = new ArrayList<>();
+        
+        for(int i = 0; i < 200; i++) {
+            cells.add(new Cell(
+                rand.nextDouble() * (canvas.getWidth() - size),
+                rand.nextDouble() * (canvas.getWidth() - size),
+                size,
+                Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1)));
+            canvas.add(cells.get(i).getShape());
+        }
     }
 
     private static double sqr(double x) {
